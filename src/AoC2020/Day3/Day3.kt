a9cca4fe5845522cdd9502709453501ece899c24
Oscar Forss <oscar.forss.1@gmail.com>
2021-01-15 11:42:13 +0100

    added kotlin solution

--- src/AoC2020/Day3/Day3.kt
+++ src/AoC2020/Day3/Day3.kt
@@ -3,6 +3,7 @@ package AoC2020.Day3
 import java.io.File
 
 val input = File("src/AoC2020/Day3/day3.txt").readLines()
+val list = dubbleList(input)
 
 fun dubbleList(list: List<String>): List<String> {
     val temp: MutableList<String> = mutableListOf()
@@ -11,7 +12,6 @@ fun dubbleList(list: List<String>): List<String> {
 }
 
 fun part1(): Int{
-    val list = dubbleList(input)
     var count = 0
     var treeCount = 0
     list.forEach { s -> if (s[count] =='#') treeCount++; count+=3 }
@@ -19,13 +19,11 @@ fun part1(): Int{
 }
 
 fun part2(): Long{
-    val list = dubbleList(input)
     return  goDown(1,list,1) *
             goDown(3,list,1) *
             goDown(5,list, 1) *
             goDown(7, list, 1) *
             goDown(1, list, 2)
-
 }
 
 fun goDown(toTheRight: Int, list: List<String>, down: Int): Long{
